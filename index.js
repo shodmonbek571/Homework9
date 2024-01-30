b3bc6bab85aa609514fa75f25cf145cc164f9642
shodmonbek571 <shodmonbekakbarov820@gmail.com>
2024-01-30 23:27:38 +0500

    homework

---
+++ index.js
@@ -0,0 +1,67 @@
+// let sum = 0;
+// let arr = [1, 2, 3, 4, 5, 6, 7 ];
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+    
+// }
+// console.log(sum);
+
+// console.log(arr);
+
+//   1 - MASALA
+// let array = [6, 4, 3, 7, 9, 8, 7, 5];
+// function kichikElement(arr) {
+//   arr.sort((a, b) => a - b);
+//   return arr[0];
+// }
+// console.log(kichikElement(array));
+
+//   2 - MASALA
+// let sum = 0;
+// let counter = 0;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 4, 5, 2];
+// for (let i = 1; i <= arr.length; i+=2) {
+//   if (i % 2 == 1) {
+//     sum++;
+//     counter+=i;
+//   } else {
+//     console.log("Toq son emas");
+//   }
+// }
+// console.log(sum);
+
+//    3 - MASALA
+// function tubSon(num) {
+//   if (num <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function tubSonlar(arr) {
+//   return arr.filter(num => tubSon(num));
+// }
+// let array = [4, 7, 11, 16, 17, 18, 23];
+// let natija = tubSonlar(array);
+// console.log(natija); 
+
+//  4 - MASALA
+// let sum = 0;
+// let arr = [1, 4, 3, 28, 9, 15, 27 ];
+// function karralilar(arr) {
+//   for (let i = 0; i <= arr.length; i++) {
+//     if ([i] % 3 == 0) {
+//       sum++;
+//     }
+    
+//   }
+//   return sum;
+// }
+// console.log(karralilar(arr));
+
+
